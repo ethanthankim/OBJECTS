08637f00eab090b5f741dcb956d4952231b718f8
ethanthankim <ethan.kim1996@gmail.com>
2024-07-20 21:40:17 -0400

    build test

---
+++ build/precache-manifest.d5f90198ac0493470614c2a2c39ca932.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979990ea185f4afa142ad51fbc78dc09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d8cf5992433beaebb5f",
+    "url": "/static/css/main.45f549b2.chunk.css"
+  },
+  {
+    "revision": "401311cfa101d2c25161",
+    "url": "/static/js/2.ff3d6294.chunk.js"
+  },
+  {
+    "revision": "29fe86816a802490cec5",
+    "url": "/static/js/3.d3e4d6ea.chunk.js"
+  },
+  {
+    "revision": "2d8cf5992433beaebb5f",
+    "url": "/static/js/main.c2dd3e21.chunk.js"
+  },
+  {
+    "revision": "4177e1f958980034a540",
+    "url": "/static/js/runtime~main.ebf801e5.js"
+  },
+  {
+    "revision": "1c35c17b8173d3f0476b987c4267a524",
+    "url": "/static/media/Baskervville-Italic.1c35c17b.ttf"
+  },
+  {
+    "revision": "0dc261498ffe8a08b08e0bd1d08f3217",
+    "url": "/static/media/Baskervville-Regular.0dc26149.ttf"
+  },
+  {
+    "revision": "714b7b9d4a28501fd0959003b0a5742d",
+    "url": "/static/media/Birdie.714b7b9d.otf"
+  },
+  {
+    "revision": "a19272e7241bdc551f153b6dd486e053",
+    "url": "/static/media/Dione.a19272e7.png"
+  },
+  {
+    "revision": "6daf39d4b07345e70d9cd116de099ce7",
+    "url": "/static/media/Dione1.6daf39d4.png"
+  },
+  {
+    "revision": "e9628e8976e81b072c0eb739b9f9557b",
+    "url": "/static/media/Dione2.e9628e89.png"
+  },
+  {
+    "revision": "d9da1f1771f9f6855f8c83afc244933b",
+    "url": "/static/media/Dione3.d9da1f17.png"
+  },
+  {
+    "revision": "a64b5db9f8cfc76b8d126477911a1b2c",
+    "url": "/static/media/Dione4.a64b5db9.png"
+  },
+  {
+    "revision": "50c1fe474aab5cecc00a2ae63d14a8cf",
+    "url": "/static/media/FuturaCyrillicBold.50c1fe47.ttf"
+  },
+  {
+    "revision": "cc17f68d3bbbdd07678bc70472ac6523",
+    "url": "/static/media/FuturaCyrillicBook.cc17f68d.ttf"
+  },
+  {
+    "revision": "1417bc6528fc33f47e148ac12b682d68",
+    "url": "/static/media/FuturaCyrillicLight.1417bc65.ttf"
+  },
+  {
+    "revision": "893a0b0d3130e3b0499e0508f7452246",
+    "url": "/static/media/Jado.893a0b0d.png"
+  },
+  {
+    "revision": "8644f7c8cee4aee2d38cde8c9629adf0",
+    "url": "/static/media/Jado1.8644f7c8.png"
+  },
+  {
+    "revision": "91fe28c38e65b4e2ce41d488fffc4020",
+    "url": "/static/media/Jado2.91fe28c3.png"
+  },
+  {
+    "revision": "3963dcfc8a5b41e76f791627ece755af",
+    "url": "/static/media/KaWai.3963dcfc.png"
+  },
+  {
+    "revision": "236efa058a7e0fd172f0b84236d0c111",
+    "url": "/static/media/KaWai1.236efa05.png"
+  },
+  {
+    "revision": "ac1dcadc9ed69d3a91ee1c48daccf2e7",
+    "url": "/static/media/KaWai2.ac1dcadc.png"
+  },
+  {
+    "revision": "0a14a474e50b6e018c481b02382e2442",
+    "url": "/static/media/KaWai3.0a14a474.png"
+  },
+  {
+    "revision": "3db244356f1e1767a717c58869677dd5",
+    "url": "/static/media/KaWai4.3db24435.png"
+  },
+  {
+    "revision": "0c03201a0a3c4e391636c6ae71552b22",
+    "url": "/static/media/MailartRubberstamp-Regular.0c03201a.otf"
+  },
+  {
+    "revision": "1b5d4ed1dac8073060f75424112b122c",
+    "url": "/static/media/bell.1b5d4ed1.png"
+  },
+  {
+    "revision": "6fac6cbace702dacb757d2eb1d8f7c10",
+    "url": "/static/media/elephant.6fac6cba.png"
+  },
+  {
+    "revision": "89fe4a4a99ae8498c822eaf4a0918d03",
+    "url": "/static/media/jewelry.89fe4a4a.png"
+  },
+  {
+    "revision": "5ba02d4c07d93db38964a2c06547d9e8",
+    "url": "/static/media/keyhole.5ba02d4c.png"
+  },
+  {
+    "revision": "e79bf2beb0dcb368730bce38955a4aa0",
+    "url": "/static/media/medal.e79bf2be.png"
+  },
+  {
+    "revision": "297b7ec27e62d7e37a8ea89050e3f583",
+    "url": "/static/media/packing.297b7ec2.png"
+  },
+  {
+    "revision": "80909b58d87a26c9980fdfc36af1ec6d",
+    "url": "/static/media/sewing.80909b58.png"
+  },
+  {
+    "revision": "8b5e5a594c7e99854e2310671cc7e396",
+    "url": "/static/media/watch.8b5e5a59.png"
+  }
+]);
