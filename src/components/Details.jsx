e5b3cf1963c3d4984a6640043a18f3cd7191885f
ethanthankim <ethan.kim1996@gmail.com>
2024-07-21 09:40:55 -0400

    added pause button

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -2,6 +2,8 @@
 import React, { useEffect, useState, useRef, useReducer } from "react"
 import Navbar from "./Navbar"
 import Layout from "./layouts/Layout"
+import playImage from "../images/play.png"
+import pauseImage from "../images/pause.png"
 
 export default function Details(props) {
     function findSelected(objArray) {
@@ -33,14 +35,22 @@ export default function Details(props) {
     const audioFile = `${process.env.PUBLIC_URL}/audio/${selected.audio}`;
     const audioRef = useRef(new Audio(audioFile))
 
-    const togglePlay = () => {
+    function togglePlay(id) {
         const audio = audioRef.current
+        const button = document.getElementById(id);
+        const buttonText = button.querySelector("#button--text");
+        const buttonImage = button.querySelector(".audio--image");
+
         if (!isPlaying) {
-        audio.play();
-        setIsPlaying(true);
+            audio.play();
+            setIsPlaying(true);
+            buttonText.innerHTML = "pause audio";
+            buttonImage.src = pauseImage;
         } else {
-        audio.pause();
-        setIsPlaying(false);
+            audio.pause();
+            setIsPlaying(false);
+            buttonText.innerHTML = "play audio";
+            buttonImage.src = playImage;
         }
     };
 
@@ -55,9 +65,9 @@ export default function Details(props) {
                 />
                 {/* Conditionally render audio button and quote section if they exist */}
                 { selected.audio &&
-                <button className="audio--button" onClick={togglePlay}>
-                    <img src={require("../images/play.png")} className="audio--image"></img>
-                    play audio
+                <button className="audio--button" id="audio--play" onClick={() => togglePlay("audio--play")}>
+                    <img src={playImage} className="audio--image" />
+                    <span id="button--text">play audio</span>
                 </button>
                 }
                 {selected.quote &&
