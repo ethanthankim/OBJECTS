62b0ed6415de8da1e1671a5cb642d80eb61aea7d
ethanthankim <ethan.kim1996@gmail.com>
2024-07-21 21:52:45 -0400

    added photos to last stories

--- src/components/layouts/Hayam.jsx
+++ src/components/layouts/Hayam.jsx
@@ -4,8 +4,22 @@ export default function Hayam(props) {
     const {story} = props
     return (
         <section>
-            <div className="section--fulltext">
-                {story}
+            <div className="section">
+                <div className="section--text">
+                    {story}
+                </div>
+                <div className="section--gallery">
+                    <img 
+                        src={require("../../photos/Hayam/Hayam1.png")} 
+                        alt="sewing-machine-top" 
+                        className="section--photo"
+                    />
+                    <img 
+                        src={require("../../photos/Hayam/Hayam2.png")} 
+                        alt="sewing-machine-side" 
+                        className="section--photo"
+                    />
+                </div>
             </div>
         </section>
     )
--- src/components/layouts/Mariam.jsx
+++ src/components/layouts/Mariam.jsx
@@ -5,8 +5,24 @@ export default function Mariam(props) {
     return (
         <section>
             <div className="section--fulltext">
-                {story}
+                {story.slice(0,3)}
             </div>
+            <div className="section--row">
+                <img 
+                    src={require("../../photos/Mariam/Mariam1.png")} 
+                    alt="jewelry-box-outer" 
+                    className="row--photo"
+                />
+                <img 
+                    src={require("../../photos/Mariam/Mariam2.png")} 
+                    alt="jewelry-box-inner" 
+                    className="row--photo"
+                />
+            </div>
+            <div className="section--fulltext">
+                {story.slice(3)}
+            </div>
+
         </section>
     )
 }
